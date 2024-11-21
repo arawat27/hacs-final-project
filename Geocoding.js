134de24ee71866ab372aa2dd4cc70cb3d050c9a1
ethanngyen <ahn008huy@gmail.com>
2024-11-21 12:03:53 -0700

    Geocoding Application Component
    This component grabs and displays user location after they click a button.

---
+++ Geocoding.js
@@ -0,0 +1,59 @@
+//Necessary import for geolocation feature
+import React, { useState } from 'react';
+
+function Geocoding() {
+  //Geolocation methods
+  const [userLocation, setUserLocation] = useState();
+  
+  //get user's geolocation and update userLocation
+  const getUserLocation = () => {
+    
+    //check whether geolocation feature is supported
+    if (navigator.geolocation) {
+      
+      //retrieve user location
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+
+          //access coords in the position variable
+          const { latitude, longitude } = position.coords;
+
+          //update userLocation
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          
+          //display an error message if not supported
+          console.log("Error getting user location: ", error);
+        }
+      );
+
+    }
+    else {
+      //display an error message if not supported
+      console.log("Geolocation is not supported by this browser.");
+    }
+  };
+  
+  return (
+    <div className="Geolocation">
+      <p>Hello World</p>
+      
+      {/* Button to get user geolocation */}
+      <button onClick = { getUserLocation }>Get User geolocation</button>
+
+      {/* Display user location is userLocation is not NULL */}
+      {
+        userLocation && (
+          <div>
+            <h2>User Location</h2>
+            <p>Latitude: {userLocation.latitude}</p>
+            <p>Longitude: {userLocation.longitude}</p>
+          </div>
+        
+      )}
+    </div>
+  );
+}
+
+export default Geocoding;
